c0f2622fd293952180f2f23cb4b756d6fd80e212
mspertus <mike@spertus.com>
2019-11-17 10:59:21 -0600

    Modify mpcs53014 to be based on mpcs53014_hive

--- docker/mpcs53014/init_container.sh
+++ docker/mpcs53014/init_container.sh
@@ -39,7 +39,7 @@ if [ ! -e $CONTAINER_ALREADY_INITIALIZED ]; then
 	sudo -u hadoop -H sh -c "echo executing hdfs dfs -put /home/hadoop/user/hive/warehouse/* /user/hive/warehouse;  /home/hadoop/hadoop/bin/hdfs dfs -put /home/hadoop/user/hive/warehouse/* /user/hive/warehouse"
 	sudo -u hadoop -H sh -c "echo executing rm -r /home/hadoop/user;  rm -r /home/hadoop/user"
 	sudo -u hadoop -H sh -c "echo executing hiveserver2; cd; /home/hadoop/hive/bin/hiveserver2 &"
-	sudo -u hadoop -H sh -c "echo Giving hiveserver2 time; sleep 100"
+	sudo -u hadoop -H sh -c "echo Giving hiveserver2 time; sleep 180"
 	sudo -u hadoop -H sh -c "echo executing beeline; cd; /home/hadoop/spark/bin/beeline -u jdbc:hive2://localhost:10000/default -f init_tables.hql"
 
 	sudo -u hadoop -H sh -c "echo executing stop-all.sh; cd; /home/hadoop/hadoop/sbin/stop-all.sh"
